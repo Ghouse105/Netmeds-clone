efbc3b83755c68e7fd0e71e24d3eed02dedb6a23
Ghouse105 <afghanghousekkl@gmail.com>
2022-02-25 20:19:08 +0530

    Done

---
+++ product.js
@@ -0,0 +1,511 @@
+var data = [
+
+    {
+      image:
+        "https://www.netmeds.com/images/product-v1/150x150/100426/2baconil_tts30_21mg_patch_7_s_0.jpg",
+  
+      name: "2baconil TTS30 21mg Patch 7'S",
+  
+      price: 672,
+      id: Math.random(),
+    },
+    {
+      image:
+          "https://www.netmeds.com/images/product-v1/150x150/971795/gemblue_biocare_spa_body_care_body_lotion_500_ml_0_0.jpg",
+
+       name:"Gemblue Biocare SPA Body Care Body lotion 500 ml",
+       
+       price:"198",
+       id: Math.random(),
+    },
+    {
+      image:
+         "https://www.netmeds.com/images/product-v1/150x150/914291/mothercare_toddler_silicone_crumb_catcher_bibs_pink_pack_of_2_0_0.jpg",
+
+         name:"Mothercare Toddler Silicone Crumb Catcher Bibs",
+
+         price: 1399,
+         id: Math.random(),
+    },
+    {
+      image:
+        "https://www.netmeds.com/images/product-v1/150x150/412078/eno_fruit_salt_powder_orange_flavour_pack_of_30_x_5_gm_0.jpg",
+  
+      name: "Eno Fruit Salt Powder - Orange Flavour (Pack of 30 x 5 gm)",
+  
+      price: 270,
+      id: Math.random(),
+    },
+    {
+      image:
+         "https://www.netmeds.com/images/product-v1/150x150/959720/hashmi_sikander_e_azam_plus_capsule_10s_0_0.jpg",
+
+         name:"Hashmi Sikander-e-Azam Plus Capsule 10's",
+
+         price: 1286 ,
+         id: Math.random(),
+    },
+    {
+      image:
+        "https://www.netmeds.com/images/product-v1/150x150/958409/status_n95_face_mask_white_pack_of_20_0_1.jpg",
+  
+      name: "Status N95 Face Mask - White (Pack of 20)",
+  
+      price: 200,
+      id: Math.random(),
+       
+    },
+    {
+      image:
+         "https://www.netmeds.com/images/product-v1/150x150/15590/omron_automatic_blood_pressure_monitor_hem_7120_0_2.jpg",
+
+         name:"Omron Automatic Blood Pressure Monitor",
+
+         price:  1864,
+         id: Math.random(),
+    },
+    {
+      image:
+         "https://www.netmeds.com/images/product-v1/150x150/821307/kapiva_wheat_grass_juice_1_ltr_0_2.jpg",
+
+      name:"Kapiva Wheat Grass Juice 1 ltr",
+      
+      price: 469,
+      id: Math.random(),
+    },
+    {
+      image:
+        "https://www.netmeds.com/images/product-v1/150x150/912261/ciphands_daily_surface_disinfectant_spray_75_ml_0_3.jpg",
+  
+      name: "Ciphands Daily Surface Disinfectant Spray 75 ml",
+  
+      price: 94,
+      id: Math.random(),
+  
+      
+    },
+    {
+      image:
+         "https://www.netmeds.com/images/product-v1/150x150/408251/omron_blood_pressure_cuff_hem_cl24_l_0.jpg",
+
+         name:"Omron Blood Pressure Cuff (HEM-CL24) (L)",
+
+         price:  750,
+         id: Math.random(),
+    },
+    {
+      image:
+        "https://www.netmeds.com/images/product-v1/150x150/953905/shake_handz_e_hand_rub_5_litre_0_0.jpg",
+  
+      name: "Shake HandZ-E Alcohol Hand Rub 5 litre",
+  
+      price: 575,
+  
+      
+    },
+    {
+      image:
+        "https://www.netmeds.com/images/product-v1/150x150/408264/omron_thermometer_pencil_type_mc_246_c1_0.jpg",
+  
+      name: "Omron Thermometer Pencil Type (MC-246-C1)",
+  
+      price: 200,
+  
+      
+    },
+  
+    {
+      image:
+        "https://www.netmeds.com/images/product-v1/150x150/902225/sahyog_wellness_digital_thermometer_0_0.jpg",
+  
+      name: "Sahyog Wellness Digital Thermometer",
+  
+      price: 110,
+  
+      
+    },
+    {
+      image:
+         "https://www.netmeds.com/images/product-v1/150x150/985297/univen_powder_free_nitrile_gloves_m_100s_0_1.jpg",
+
+         name:"UniVen Powder Free Nitrile Gloves (M) 100's",
+
+         price:  800,
+    },
+    {
+      image:
+        "https://www.netmeds.com/images/product-v1/150x150/920731/ourdaily_vitamin_c_zinc_tablet_15s_0_2.jpg",
+  
+      name: "Ourdaily Vitamin C & Zinc Tablet 15's",
+  
+      price: 40,
+  
+      
+    },
+    {
+      image:
+        "https://www.netmeds.com/images/product-v1/150x150/912066/askmed_active_surface_disinfectant_spray_300_ml_0_0.jpg",
+  
+      name: "Askmed Active Surface Disinfectant Spray 300 ml",
+  
+      price: 100,
+  
+      
+    },
+    {
+      image:
+        "https://www.netmeds.com/images/product-v1/150x150/835057/dettol_antiseptic_liquid_550_ml_0.jpg",
+  
+      name: "Dettol Antiseptic Liquid 550 ml",
+  
+      price: 194,
+  
+     
+    },
+    {
+      image:
+         "https://www.netmeds.com/images/product-v1/150x150/958796/nitethru_advanced_sleep_aid_capsule_30s_0_0.jpg",
+
+         name:"Nitethru Advanced Sleep Aid Capsule 30's",
+
+         price:  236,
+    },
+    {
+      image:
+        "https://www.netmeds.com/images/product-v1/150x150/859708/omron_compressor_nebulizer_ne_c101_0.jpg",
+  
+      name: "Omron Compressor Nebulizer (NE-C101)",
+  
+      price: 1904,
+  
+      
+    },
+  
+    {
+      image:
+        "https://www.netmeds.com/images/product-v1/150x150/974348/beurer_ear_thermometer_ft_58_1s_0_0.jpg",
+  
+      name: "Beurer Ear Thermometer (FT 58)",
+  
+      price: 1672,
+  
+
+    },
+    {
+      image:
+        "https://www.netmeds.com/images/product-v1/150x150/408712/romsons_respirometer_0.jpg",
+  
+      name: "Romsons Respirometer",
+  
+      price: 160,
+  
+      
+    },
+  
+    {
+      image:
+        "https://www.netmeds.com/images/product-v1/150x150/948692/oxy5_refillable_portable_oxygen_can_6_litre_0_1.jpg",
+  
+      name: "Oxy5 Refillable Portable Oxygen Can 6 Litre",
+  
+      price: 562,
+  
+    },
+    {
+      image:
+         "https://www.netmeds.com/images/product-v1/150x150/930167/pro360_gold_whey_100_protein_powder_chocolate_flavour_1_kg_0_0.jpg",
+
+         name:"Pro360 Gold Whey 100% Protein Powder",
+
+         price:  1699,
+    },
+    {
+      image:
+        "https://www.netmeds.com/images/product-v1/150x150/908053/soft_hands_non_sterile_latex_medical_examination_gloves_m_100s_0_0.jpg",
+  
+      name: "Soft Hands Non Sterile Latex Medical Examination",
+  
+      price: 400,
+  
+      
+    },
+    {
+      image:
+        "https://www.netmeds.com/images/product-v1/150x150/911864/medigree_protective_medical_coverall_0_0.jpg",
+  
+      name: "Medigree Protective Medical Coveral",
+  
+      price: 401,
+  
+      id: Math.random(),
+    },
+    {
+      image:
+        "https://www.netmeds.com/images/product-v1/150x150/959012/detelpro_juren_oxygen_concentrator_1_7_lpm_1s_0_0.jpg",
+  
+      name: "DetelPro Juren Oxygen Concentrator (1-7 LPM)",
+  
+      price: 3900,
+  
+      
+    },
+    {
+      image:
+         "https://www.netmeds.com/images/product-v1/150x150/930972/skin_elements_anti_chafing_cream_pack_of_20_x_15_ml_0_1.jpg",
+
+         name:"Skin Elements Anti Chafing Cream",
+
+         price: 399 ,
+    },
+    {
+      image:
+        "https://www.netmeds.com/images/product-v1/150x150/908052/soft_hands_non_sterile_latex_medical_examination_gloves_s_100s_0_0.jpg",
+  
+      name: "Soft Hands Non Sterile Latex Medical Gloves",
+  
+      price: 425,
+  
+      
+    },
+    {
+      image:
+        "https://www.netmeds.com/images/product-v1/150x150/912655/floh_non_contact_digital_infrared_thermometer_pg_irt1602_0_0.jpg",
+  
+      name: "Floh Non Contact Digital Infrared Thermometer",
+  
+      price: 1499,
+  
+    },
+    {
+      image:
+         "https://www.netmeds.com/images/product-v1/150x150/924994/skin_elements_mens_intimate_wash_menthol_120_ml_0_2.jpg",
+
+         name:"Skin Elements Men's Intimate Wash",
+
+         price: 418 ,
+    },
+    {
+      image:
+        "https://www.netmeds.com/images/product-v1/150x150/971783/gemblue_biocare_vitamin_c_cream_500_ml_0_0.jpg",
+  
+      name: "Gemblue Biocare Vitamin C Cream 500 ml",
+  
+      price: 209,
+  
+    },
+    {
+      image:
+        "https://www.netmeds.com/images/product-v1/150x150/995524/bella_vita_organic_ylang_ylang_essential_oil_15_ml_0_0.jpg",
+  
+      name: "Bella Vita Organic Ylang Ylang Essential Oil 15 ml",
+  
+      price: 299,
+  
+    },
+    {
+      image:
+         "https://www.netmeds.com/images/product-v1/150x150/937673/be_o_man_beard_mustache_foam_wash_sensual_amber_100_ml_0_3.jpg",
+
+         name:"Be O Man Beard & Mustache Wash",
+
+         price: 299 ,
+    },
+    {
+      image:
+        "https://www.netmeds.com/images/product-v1/150x150/824255/sri_sri_tattva_body_butter_150_gm_0.jpg",
+  
+      name: "Sri Sri Tattva Body Butter 150 gm",
+  
+      price: 225,
+  
+    },
+    {
+      image:
+        "https://www.netmeds.com/images/product-v1/150x150/814807/dr_vaidyas_huff_n_kuff_lozenges_50s_0_2.jpg",
+  
+      name: "Dr.Vaidya's Huff N Kuff Lozenges 50's",
+  
+      price: 87,
+  
+    },
+    {
+      image:
+        "https://www.netmeds.com/images/product-v1/150x150/15176/moov_spray_pain_relief_specialist_80_gm_0.jpg",
+  
+      name: "Moov Spray Pain Relief Specialist 80 gm",
+  
+      price: 246,
+  
+    },
+    {
+      image:
+        "https://www.netmeds.com/images/product-v1/150x150/858567/inlife_triphala_extract_capsules_60_s_0.jpg",
+  
+      name: "INLIFE Triphala Extract Capsules 60's",
+  
+      price: 247,
+  
+    },
+    {
+      image:
+        "https://www.netmeds.com/images/product-v1/150x150/959708/hashmi_hard_rock_capsule_20s_0_0.jpg",
+  
+      name: "Hashmi Hard Rock Capsule 20's",
+  
+      price: 1900,
+  
+    },
+    {
+      image:
+        "https://www.netmeds.com/images/product-v1/150x150/814310/himalaya_wellness_ashvagandha_tablet_60_s_0.jpg",
+  
+      name: "Himalaya Wellness Ashvagandha Tablet 60's",
+  
+      price: 165,
+  
+    },
+    {
+      image:
+         "https://www.netmeds.com/images/product-v1/150x150/932448/uncle_tony_beard_wash_250_ml_0_0.jpg",
+       
+      name:"Uncle Tony Beard Wash Gel 250 ml",
+
+      price: 1799,
+    },
+    {
+      image:
+         "https://www.netmeds.com/images/product-v1/150x150/994894/man_arden_premium_shaving_brush_vintage_finish_1s_0_0.jpg",
+       
+      name:"Man Arden Premium Shaving Brush",
+
+      price: 699,
+    },
+  ];
+  
+  var temp = data;
+  
+  let parent = document.getElementById("container");
+  
+  function showProduct(item) {
+    item.forEach(function (product) {
+      let div = document.createElement("div");
+      div.setAttribute("class", "item-container");
+  
+      let img = document.createElement("img");
+      img.setAttribute("class", "item-img");
+      img.src = product.image;
+  
+      let item_name = document.createElement("p");
+      item_name.innerText = product.name;
+  
+      let price = document.createElement("p");
+      price.innerText = product.price;
+  
+      let text_div = document.createElement("div");
+      text_div.setAttribute("class", "text-container");
+
+      let add_to_cart  = document.createElement("button");
+      add_to_cart.textContent = "ADD TO CART";
+  
+      add_to_cart.onclick = function () {
+        addtocart(product);
+      };
+  
+      let img_div = document.createElement("div");
+      img_div.setAttribute("class", "img-container");
+      img_div.append(img, add_to_cart);
+  
+      // let btn_div = document.createElement("div");
+  
+      // btn_div.append(add_to_cart)
+  
+      text_div.append(item_name, price);
+  
+      div.append(img_div, text_div, add_to_cart);
+  
+      parent.append(div);
+    });
+  }
+  showProduct(data);
+  
+  function sorting() {
+    let select = document.getElementById("sort");
+  
+    let value = select.options[select.selectedIndex].value;
+  
+    // alert(value);
+  
+    if (value === "price-low") {
+      sortLH();
+    } else if (value === "price-high") {
+      sortHL();
+    } else if (value === "a-z") {
+      sortAZ();
+    } else if (value === "z-a") {
+      sortZA();
+    } else if (value === "new-arrival") {
+      sortNew();
+    }
+  }
+  
+  function sortLH() {
+    let items = data.sort(function (a, b) {
+      return Number(a.price) - Number(b.price);
+      // console.log(Number(a.price))
+    });
+    parent.innerHTML = null;
+    showProduct(items);
+  }
+  
+  function sortHL() {
+    let items = data.sort(function (a, b) {
+      return Number(b.price) - Number(a.price);
+      // console.log(Number(a.price))
+    });
+    parent.innerHTML = null;
+    showProduct(items);
+  }
+  
+  function sortAZ() {
+    let items = data.sort(function (a, b) {
+      return a.name.localeCompare(b.name);
+    });
+    parent.innerHTML = null;
+    showProduct(items);
+  }
+  
+  function sortZA() {
+    let items = data.sort(function (a, b) {
+      return b.name.localeCompare(a.name);
+    });
+    parent.innerHTML = null;
+    showProduct(items);
+  }
+  
+  function sortNew() {
+    let items = data.sort(function (a, b) {
+      return a.id - b.id;
+      // console.log(a.id.getTime(), b.id.getTime())
+    });
+    parent.innerHTML = null;
+    showProduct(items);
+  }
+  
+  if (localStorage.getItem("cart") === null) {
+    localStorage.setItem("cart", JSON.stringify([]));
+  }
+  
+  function addtocart(p) {
+    let products_cart = JSON.parse(localStorage.getItem("cart"));
+  
+    products_cart.push(p);
+  
+    localStorage.setItem("cart", JSON.stringify(products_cart));
+  
+    alert("Product successfully added to cart")
+  }
+
+  let parent1 = document.getElementById("container1")
+  
+  let proceed_to_cart = document.createElement("button");
+  procced_to_cart.text = "Proceed To Cart";
